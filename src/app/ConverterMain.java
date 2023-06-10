c200aacd0c44dad908c10506ce119f99b0928999
diodar <mikhailgalis@gmail.com>
2023-06-10 13:39:12 +0300

    Change info. Add convert method mi-km

--- src/app/ConverterMain.java
+++ src/app/ConverterMain.java
@@ -2,6 +2,13 @@ package app;
 
 public class ConverterMain {
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        double mi = 1287.5;
+        System.out.println("Distance from NewYork to Miami in miles: " + mi);
+        double distanceInKm = convertMilesToKms(mi);
+        System.out.println("Distance from NewYork to Miami in kilometres: " + distanceInKm);
+    }
+
+    private static double convertMilesToKms(double mi) {
+        return mi * 1.60934;
     }
 }
