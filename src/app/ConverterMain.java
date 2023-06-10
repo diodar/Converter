c368c61b7576ec38f00dd55e1fb1c625a815adb2
diodar <mikhailgalis@gmail.com>
2023-06-10 13:31:48 +0300

    Add convert method km-mi

--- src/app/ConverterMain.java
+++ src/app/ConverterMain.java
@@ -4,5 +4,10 @@ public class ConverterMain {
     public static void main(String[] args) {
         double km = 473;
         System.out.println("Distance from Kyiv to Odesa in kilometres: " + km);
+        double distanceInMi = convertKmsToMiles(km);
+        System.out.println("Distance from Kyiv to Odesa in miles: " + distanceInMi);
+    }
+    private static double convertKmsToMiles(double km) {
+        return km / 1.60934;
     }
 }
